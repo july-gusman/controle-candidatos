7b831ef57c27c3e310e9b3b80da94d7da30973ab
july-gusman <julianagusman95@gmail.com>
2024-06-01 11:57:19 -0300

    processo seletivo

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,21 +1,45 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
-    public static void main(String[] args) throws Exception {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
-
+    public static void main(String[] args) {
+        selecaoCandidatos();
     }
-    static void analisarCandidato(double salarioPretendido) {
-          double salarioBase =  2000.0;
-          if(salarioBase > salarioPretendido) {
-               System.out.println("LIGAR PARA O CANDIDATO");
-          } else if (salarioBase ==salarioPretendido){
-             System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
-           } else {
-                System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
-             }
 
-      }
+    static void selecaoCandidatos() {
+        String[] candidatos = {"FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        // Gera um valor de salário aleatório entre 1800 e 2200
+        return ThreadLocalRandom.current().nextDouble(1800, 2201);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
 }
